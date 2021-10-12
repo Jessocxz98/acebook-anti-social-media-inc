336beb3eeb9f7f7324890a4ece1ef67cde0caf58
Gianluca <gianluca.ansaldi.1989@gmail.com>
2021-10-12 14:31:31 +0100

    created User model, validation and secure password, bundled bcrypt

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,12 @@
 class User < ApplicationRecord
   has_many :posts
+  has_secure_password
+
+  VALID_EMAIL = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  validates :username, presence: true, uniqueness: true, length: {minimum: 3, maximum: 25}
+  validates :email, presence: true, uniqueness: true, format: {with: VALID_EMAIL}, length: {maximum: 120}
+  validates :password, presence: true
+
+  before_save { self.email = email.downcase }
 end
