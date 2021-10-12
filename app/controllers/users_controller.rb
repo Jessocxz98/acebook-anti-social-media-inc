6440620182c1a0a122a2fac4df11fc3d2b748f88
Gianluca <gianluca.ansaldi.1989@gmail.com>
2021-10-12 15:34:27 +0100

    created user controller and routes

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,28 @@
+class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(post_params)
+    if @user.save
+      flash[:notice] = 'Welcome to the app'
+      redirect_to posts_url
+    else
+      redirect_to signup_url
+      flash[:notice] = "Something wrong happened"
+    end
+  end
+
+  def index
+    @user = User.all
+  end
+
+  private
+
+  def post_params
+    params.require(:user).permit(:username, :email, :password)
+  end
+
+end
