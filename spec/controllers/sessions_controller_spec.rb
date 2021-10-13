6271dfff9d09e83d2a8c49d16aca9109efa071b1
Mabon ap Gwyn <mabonapgwyn@hotmail.co.uk>
2021-10-13 11:11:08 +0100

    fixed tests

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,9 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe SessionsController, type: :controller do
-  before do
-    user = User.create(username: "test", email: "test@example.com", password: "password")
-  end
+  
 
   describe "GET /session/new" do
     it "responds with 200" do
@@ -13,14 +11,18 @@ RSpec.describe SessionsController, type: :controller do
   end
 
   describe "POST /login" do
-    it "responds with 200" do
-      post :create, params: { email: "test@example.com", password: "password" }
-      expect(response).to redirect_to(posts_url)
+    
+
+    it "session id matches with created user ID" do
+      user = User.create(username: "test", email: "test@example.com", password: "password")
+      post :create, params: { session: { email: "test@example.com", password: "password" } }
+      expect(session[:user_id]).to eq user.id
     end
 
     it "can't create session" do
+      User.create(username: "test", email: "test@example.com", password: "password")
       post :create, params: { session: { email: "test1@example.com", password: "password" } }
-      expect(response).to redirect_to(login_url)
+      expect(session[:user_id]).not_to be
     end
   end
 end
