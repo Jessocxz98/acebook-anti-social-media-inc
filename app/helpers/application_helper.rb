a1570d2f4049e5e369772740ae27bb881242bc72
Sophie Bonneval <maldorana@gmail.com>
2021-10-14 16:42:33 +0100

    Avatar default

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+
+  def avatar_display(size)
+    if current_user
+      if current_user.avatar.attached?
+	      image_tag current_user.avatar, style: "width: #{size}px; display: block"
+      else
+        image_tag "/app/assets/images/svg/default_avatar.svg", style: "width: #{size}px; display: block"
+      end
+    end
+  end
+
+  def show_svg(path)
+    File.open("app/assets/images/svg/#{path}", "rb") do |file|
+      raw file.read
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
 
   VALID_EMAIL = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
+  validates :avatar, blob: { content_type: :image, size_range: 1..(5.megabytes) }
   validates :username, presence: true, uniqueness: true, length: {minimum: 3, maximum: 25}
   validates :email, presence: true, uniqueness: true, format: {with: VALID_EMAIL}, length: {maximum: 120}
   validates :password, presence: true
