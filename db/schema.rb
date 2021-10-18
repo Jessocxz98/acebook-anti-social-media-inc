a8b7950d40ace2c767ddb59b0b3d1e95c7594812
tj <tomisinjoseph3@gmail.com>
2021-10-18 12:19:20 +0100

    Added comment form to posts

--- db/schema.rb
+++ db/schema.rb
@@ -34,11 +34,11 @@ ActiveRecord::Schema.define(version: 2021_10_14_142722) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
     t.string "username"
     t.string "email"
     t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   add_foreign_key "comments", "posts"
