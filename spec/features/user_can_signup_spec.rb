758bd891ed14217c8def7028ad8b3fcf96172045
Gianluca <gianluca.ansaldi.1989@gmail.com>
2021-10-19 16:03:41 +0100

    added feature test for user signup

--- spec/features/user_can_signup_spec.rb
+++ spec/features/user_can_signup_spec.rb
@@ -1,3 +1,14 @@
 require 'rails_helper'
 
 RSpec.feature "Timeline", type: :feature do
+  scenario "user can signup" do
+    visit "/"
+    click_link "Log in"
+    click_link "Sign up"
+    fill_in "username", with: "Bob"
+    fill_in "email", with: "Bob@test.com"
+    fill_in "password", with: "123456"
+    click_button "Sign up"
+    expect(page).to have_content("Welcome to Acebook, Bob")
+  end
+end
