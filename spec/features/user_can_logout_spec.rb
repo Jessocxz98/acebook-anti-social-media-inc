3900de234283eb02f54d517183210c0020c92010
Gianluca <gianluca.ansaldi.1989@gmail.com>
2021-10-19 16:45:40 +0100

    feature test for logout and add post

--- spec/features/user_can_logout_spec.rb
+++ spec/features/user_can_logout_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.feature "Timeline", type: :feature do
-  scenario "user can signup" do
+  scenario "user can logout" do
     visit "/"
     click_link "Log in"
     click_link "Sign up"
@@ -9,7 +9,7 @@ RSpec.feature "Timeline", type: :feature do
     fill_in "email", with: "Bob@test.com"
     fill_in "password", with: "123456"
     click_button "Sign up"
-    expect(page).to have_content("Welcome to Acebook, Bob")
+    click_link "Log out"
+    expect(page).to have_content("You are now logged out")
   end
 end
-
