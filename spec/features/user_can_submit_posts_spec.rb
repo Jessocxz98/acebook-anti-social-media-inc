85cdfffbb4982511bc9ed5f1919fe232900ac900
Gianluca <gianluca.ansaldi.1989@gmail.com>
2021-10-19 16:50:26 +0100

    fixed bugs

--- spec/features/user_can_submit_posts_spec.rb
+++ spec/features/user_can_submit_posts_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.feature "Timeline", type: :feature do
-  scenario "user can signup" do
+  scenario "Can submit posts and view them" do
     visit "/"
     click_link "Log in"
     click_link "Sign up"
@@ -9,7 +9,9 @@ RSpec.feature "Timeline", type: :feature do
     fill_in "email", with: "Bob@test.com"
     fill_in "password", with: "123456"
     click_button "Sign up"
-    expect(page).to have_content("Welcome to Acebook, Bob")
+    click_link "Add post"
+    fill_in "Message", with: "Bob's your uncle!"
+    click_button "Add post"
+    expect(page).to have_content("Bob's your uncle!")
   end
 end
-
