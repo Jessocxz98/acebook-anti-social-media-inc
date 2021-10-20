1ffe500c28a9ade320f0105ac5ba35e2f511f3bc
Gianluca <gianluca.ansaldi.1989@gmail.com>
2021-10-20 12:27:47 +0100

    feature test for removing a like from a post

---
+++ spec/features/user_can_remove_a_like_from_a_post_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.feature "Timeline", type: :feature do
+  scenario "Can submit posts and view them" do
+    visit "/"
+    click_link "Log in"
+    click_link "Sign up"
+    fill_in "username", with: "Bob"
+    fill_in "email", with: "Bob@test.com"
+    fill_in "password", with: "123456"
+    click_button "Sign up"
+    click_link "Add post"
+    fill_in "Message", with: "Bob's your uncle!"
+    click_button "Add post"
+    click_button "Like"
+    click_button "Like"
+    expect(page).to have_content("0 likes")
+  end
+end
