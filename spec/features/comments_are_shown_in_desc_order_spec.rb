79ce51d0d8584b8fc126c1af207ad2f6c40e1856
Gianluca <gianluca.ansaldi.1989@gmail.com>
2021-10-20 15:00:17 +0100

    feature test to check that comments are shown in descending order

---
+++ spec/features/comments_are_shown_in_desc_order_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.feature "Timeline", type: :feature do
+  scenario "Can submit posts and view them" do
+    signup("Bob")
+    click_link "Add post"
+    fill_in "Message", with: "Bob's your uncle!"
+    click_button "Add post"
+    fill_in "comment[content]", with: "First comment"
+    click_button "Add comment"
+    fill_in "comment[content]", with: "Second comment"
+    click_button "Add comment"
+    expect("Second comment").to appear_before("First comment")
+  end
+end
