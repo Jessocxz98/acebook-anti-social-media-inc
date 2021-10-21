c06537b7f1b4ea23ef19c0dd7b667011a0416ee0
Gianluca <gianluca.ansaldi.1989@gmail.com>
2021-10-22 00:44:13 +0100

    fixed feature tests on Likes and Comments after the latest changes to js

--- spec/features/comments_are_shown_in_desc_order_spec.rb
+++ spec/features/comments_are_shown_in_desc_order_spec.rb
@@ -6,10 +6,11 @@ RSpec.feature "Timeline", type: :feature do
     click_link "Add post"
     fill_in "Message", with: "Bob's your uncle!"
     click_button "Add post"
+    click_button "comment-button"
     fill_in "comment[content]", with: "First comment"
-    click_button "Add comment"
+    click_button "add-comment-button"
     fill_in "comment[content]", with: "Second comment"
-    click_button "Add comment"
+    click_button "add-comment-button"
     expect("Second comment").to appear_before("First comment")
   end
 end
--- spec/features/user_can_add_a_comment_spec.rb
+++ spec/features/user_can_add_a_comment_spec.rb
@@ -6,8 +6,9 @@ RSpec.feature "Timeline", type: :feature do
     click_link "Add post"
     fill_in "Message", with: "Bob's your uncle!"
     click_button "Add post"
+    click_button "comment-button"
     fill_in "comment[content]", with: "and this is a comment"
-    click_button "Add comment"
+    click_button "add-comment-button"
     expect(page).to have_content("and this is a comment")
   end
 end
